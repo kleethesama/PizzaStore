c7f3dae0c8350ac90915a1f1686821fd98b9369c
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-02-25 11:14:12 +0100

    Created base item class and inherited pizza class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
-﻿class Program
+﻿public class Item
+{
+    public string Name {get; set;}
+    public int Price {get; set;}
+
+    public Item(string name, int price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
+
+public class Topping : Item
+{
+    public Topping(string name, int price) : base(name, price)
+    {
+    }
+}
+
+// public class Pizza
+// {
+//     public int MenuNumber {get; set;}
+//     public Topping[] topping {get; set;}
+// }
+
+class Program
 {
     static void Main(string[] args)
     {
