4cc1ef9567d3bdd32583fe8d177f0d9484a10d7c
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-02-26 17:27:28 +0100

    Two Store classes all of the sudden??

--- Program.cs
+++ Program.cs
@@ -155,57 +155,6 @@ public class Store
     }
 }
 
-public class Store
-{
-    public string Name {get; set;}
-    public Dictionary<string, int> PizzasAndPrices {get; set;}
-
-    public Store(string name, string[] pizzaNames, int[] pizzaPrices)
-    {
-        Name = name;
-        Dictionary<string, int> pizzasDict = new Dictionary<string, int>();
-        for (int i = 0; i < pizzaNames.Length; i++)
-        {
-            pizzasDict.Add(pizzaNames[i], pizzaPrices[i]);
-        }
-        PizzasAndPrices = pizzasDict;
-    }
-
-    public string[] GetAllPizzaNames()
-    {
-        int pizzaTypeAmount = PizzasAndPrices.Keys.Count;
-        string[] pizzaNames = new string[pizzaTypeAmount];
-        PizzasAndPrices.Keys.CopyTo(pizzaNames, 0);
-        return pizzaNames;
-    }
-
-    public Pizza PickRandomPizza()
-    {
-        string[] pizzaNames = GetAllPizzaNames();
-        int pizzaTypeAmount = PizzasAndPrices.Keys.Count;
-        Random randomizer = new Random();
-        int randomNumber = randomizer.Next(0, pizzaTypeAmount);
-        string randomPizza = pizzaNames[randomNumber];
-        int priceOfRandom = PizzasAndPrices[randomPizza];
-        return new Pizza(randomPizza, priceOfRandom, randomNumber + 1);
-    }
-
-    public void Start()
-    {
-        Pizza[] myPizzas = new Pizza[3];
-        for (int i = 0; i < myPizzas.Length; i++)
-        {
-            myPizzas[i] = PickRandomPizza();
-        }
-        Console.WriteLine($"This pizza is called {myPizzas[0].Name}, it has the menu number #{myPizzas[0].MenuNumber} and its price is {myPizzas[0].Price} DKK.");
-        Topping myTopping = new Topping("cheese", 10);
-        Console.WriteLine($"The pizza needs extra {myTopping.Name} and it'll cost an additional {myTopping.Price} DKK.");
-        myPizzas[0].AddTopping(myTopping);
-        // Customer[] customers;
-        // Order[] orders;
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
